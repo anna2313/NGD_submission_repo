9af3065cd9df8b65e7517161833401b9ed0f0256
Anna Kerekes <87497542+anna2313@users.noreply.github.com>
2026-05-06 23:48:54 +0200

    Add MIT License and copyright notice
    Added MIT License and copyright information to the file.

--- asdl_fisher.py
+++ asdl_fisher.py
@@ -1,4 +1,27 @@
-"""ASDL-backed diagonal empirical Fisher helpers.
+"""
+MIT License
+
+Copyright (c) 2020 Kazuki Osawa
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+ASDL-backed diagonal empirical Fisher helpers.
 
 The direct ASDL Fisher path computes E[g_i^2] without materializing the full
 per-sample gradient matrix. That is the expensive part of EFAdam and Fisher
